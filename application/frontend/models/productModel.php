2d9f4d96b1d4b2603bc326b57b386965a706c180
snehal trapsiya <snehal.trapsiya@gmail.com>
2015-10-14 21:02:44 +0530

    new changes

--- application/frontend/models/productModel.php
+++ application/frontend/models/productModel.php
@@ -33,4 +33,116 @@ class productModel extends Data {
 		
 		
 	}
+	
+	public function getAssignRowMaterialDetail()
+	{
+		$searchCriteria = array();
+		$searchCriteria = $this->searchCriteria;
+		
+		$selectField = "*";
+		if(isset($searchCriteria['selectField']) && $searchCriteria['selectField'] != "")
+		{
+			$selectField = 	$searchCriteria['selectField'];
+		}
+		
+		$whereClaue = "WHERE 1=1 ";
+		// By product
+		if(isset($searchCriteria['prodId']) && $searchCriteria['prodId'] != "")
+		{
+			$whereClaue .= 	" AND map.prod_id='".$searchCriteria['prodId']."' ";
+		}
+		
+		// By rawmaterial
+		if(isset($searchCriteria['rmId']) && $searchCriteria['rmId'] != "")
+		{
+			$whereClaue .= 	" AND map.rm_id='".$searchCriteria['rmId']."' ";
+		}
+		
+		$orderField = " map.id";
+		$orderDir = " ASC";
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderField']) && $searchCriteria['orderField'] != "")
+		{
+			$orderField = $searchCriteria['orderField'];
+		}
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderDir']) && $searchCriteria['orderDir'] != "")
+		{
+			$orderDir = $searchCriteria['orderDir'];
+		}
+		
+		$sqlQuery = "SELECT
+					  	".$selectField."
+					 FROM map_prod_raw_material AS map
+					 	JOIN product_master AS pm
+							ON map.prod_id = pm.prod_id
+					  	JOIN row_material_master AS rm
+							ON map.rm_id = rm.rm_id ".$whereClaue." ORDER BY ".$orderField." ".$orderDir."";
+		
+		
+		//echo $sqlQuery; exit;
+		$result     = $this->db->query($sqlQuery);		
+		$rsData     = $result->result_object();
+		
+		return $rsData;
+	}
+	
+	
+	public function getAssignProcessDetail()
+	{
+		$searchCriteria = array();
+		$searchCriteria = $this->searchCriteria;
+		
+		$selectField = "*";
+		if(isset($searchCriteria['selectField']) && $searchCriteria['selectField'] != "")
+		{
+			$selectField = 	$searchCriteria['selectField'];
+		}
+		
+		$whereClaue = "WHERE 1=1 ";
+		// By product
+		if(isset($searchCriteria['prodId']) && $searchCriteria['prodId'] != "")
+		{
+			$whereClaue .= 	" AND map.prod_id='".$searchCriteria['prodId']."' ";
+		}
+		
+		// By rawmaterial
+		if(isset($searchCriteria['procId']) && $searchCriteria['procId'] != "")
+		{
+			$whereClaue .= 	" AND map.proc_id='".$searchCriteria['procId']."' ";
+		}
+		
+		$orderField = " map.id";
+		$orderDir = " ASC";
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderField']) && $searchCriteria['orderField'] != "")
+		{
+			$orderField = $searchCriteria['orderField'];
+		}
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderDir']) && $searchCriteria['orderDir'] != "")
+		{
+			$orderDir = $searchCriteria['orderDir'];
+		}
+		
+		$sqlQuery = "SELECT
+					  	".$selectField."
+					 FROM map_prod_proc AS map
+					 	JOIN product_master AS pm
+							ON map.prod_id = pm.prod_id
+					  	JOIN process_master AS proc
+							ON map.proc_id = proc.proc_id ".$whereClaue." ORDER BY ".$orderField." ".$orderDir."";
+		
+		
+		//echo $sqlQuery; exit;
+		$result     = $this->db->query($sqlQuery);		
+		$rsData     = $result->result_object();
+		
+		return $rsData;
+	}
+	
 }
--- application/frontend/models/userModel.php
+++ application/frontend/models/userModel.php
@@ -87,6 +87,9 @@ class userModel extends Data {
 		return $rsData;
 	}
 	
+	
+	
+	
 	public function getAssignCompanyDetail()
 	{
 		$searchCriteria = array();
@@ -133,6 +136,112 @@ class userModel extends Data {
 							ON map.user_id = um.user_id
 					  	JOIN company_master AS com
 							ON map.company_id = com.com_id ".$whereClaue." ORDER BY ".$orderField." ".$orderDir."";
+		
+		//echo $sqlQuery; exit;
+		$result     = $this->db->query($sqlQuery);
+		$rsData     = $result->result_object();
+		return $rsData;
+	}
+	
+	public function getAssignDeptDetail()
+	{
+		$searchCriteria = array();
+		$searchCriteria = $this->searchCriteria;
+		
+		$selectField = "*";
+		if(isset($searchCriteria['selectField']) && $searchCriteria['selectField'] != "")
+		{
+			$selectField = 	$searchCriteria['selectField'];
+		}
+		
+		$whereClaue = "WHERE 1=1 ";
+		// By user
+		if(isset($searchCriteria['userId']) && $searchCriteria['userId'] != "")
+		{
+			$whereClaue .= 	" AND map.user_id='".$searchCriteria['userId']."' ";
+		}
+		
+		// By Company
+		if(isset($searchCriteria['deptId']) && $searchCriteria['deptId'] != "")
+		{
+			$whereClaue .= 	" AND map.dept_id='".$searchCriteria['deptId']."' ";
+		}
+		
+		$orderField = " map.id";
+		$orderDir = " ASC";
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderField']) && $searchCriteria['orderField'] != "")
+		{
+			$orderField = $searchCriteria['orderField'];
+		}
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderDir']) && $searchCriteria['orderDir'] != "")
+		{
+			$orderDir = $searchCriteria['orderDir'];
+		}
+		
+		$sqlQuery = "SELECT
+					  	".$selectField."
+					 FROM map_user_department AS map
+					 	JOIN user_master AS um
+							ON map.user_id = um.user_id
+					  	JOIN department_master AS dept
+							ON map.dept_id = dept.dept_id ".$whereClaue." ORDER BY ".$orderField." ".$orderDir."";
+		
+		//echo $sqlQuery; exit;
+		$result     = $this->db->query($sqlQuery);
+		$rsData     = $result->result_object();
+		return $rsData;
+	}
+	
+	public function getAssignStageDetail()
+	{
+		$searchCriteria = array();
+		$searchCriteria = $this->searchCriteria;
+		
+		$selectField = "*";
+		if(isset($searchCriteria['selectField']) && $searchCriteria['selectField'] != "")
+		{
+			$selectField = 	$searchCriteria['selectField'];
+		}
+		
+		$whereClaue = "WHERE 1=1 ";
+		// By user
+		if(isset($searchCriteria['utypeId']) && $searchCriteria['utypeId'] != "")
+		{
+			$whereClaue .= 	" AND map.u_typ_id='".$searchCriteria['utypeId']."' ";
+		}
+		
+		// By Company
+		if(isset($searchCriteria['stageId']) && $searchCriteria['stageId'] != "")
+		{
+			$whereClaue .= 	" AND map.ps_id='".$searchCriteria['stageId']."' ";
+		}
+		
+		$orderField = " map.id";
+		$orderDir = " ASC";
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderField']) && $searchCriteria['orderField'] != "")
+		{
+			$orderField = $searchCriteria['orderField'];
+		}
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderDir']) && $searchCriteria['orderDir'] != "")
+		{
+			$orderDir = $searchCriteria['orderDir'];
+		}
+		
+		$sqlQuery = "SELECT
+					  	".$selectField."
+					 FROM map_utype_pstage AS map
+					 	JOIN user_types AS ut
+							ON map.u_typ_id = ut.u_typ_id
+					  	JOIN process_stage_master AS ps
+							ON map.ps_id = ps.ps_id ".$whereClaue." ORDER BY ".$orderField." ".$orderDir."";
 		
 		//echo $sqlQuery; exit;
 		$result     = $this->db->query($sqlQuery);
