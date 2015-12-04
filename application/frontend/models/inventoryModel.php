445aa050457114e9b009fcceefd91f8fc81ca085
Nikunj Patel <nikunjpatel190@gmail.com>
2015-12-04 18:35:24 +0530

    nikunj

--- application/frontend/models/inventoryModel.php
+++ application/frontend/models/inventoryModel.php
@@ -10,4 +10,74 @@ class inventoryModel extends Data {
         parent::__construct();
         $this->tbl = 'inventory_master';
     }
+
+	//@Author : Nikunj Bambhroliya
+	//@Description : function will return product stock details
+	public function getInventoryDetail()
+	{
+		$searchCriteria = array();
+		$searchCriteria = $this->searchCriteria;
+		
+		$selectField = "*";
+		if(isset($searchCriteria['selectField']) && $searchCriteria['selectField'] != "")
+		{
+			$selectField = 	$searchCriteria['selectField'];
+		}
+		
+		$whereClaue = "WHERE 1=1 ";
+		
+		// By Product id
+		if(isset($searchCriteria['prod_id']) && $searchCriteria['prod_id'] != "")
+		{
+			$whereClaue .= 	" AND im.prod_id =".$searchCriteria['prod_id']." ";
+		}
+
+		// By action
+		if(isset($searchCriteria['action']) && $searchCriteria['action'] != "")
+		{
+			$whereClaue .= 	" AND im.action = '".$searchCriteria['action']."' ";
+		}
+		
+		// By Status
+		if(isset($searchCriteria['status']) && $searchCriteria['status'] != "")
+		{
+			$whereClaue .= 	" AND im.status = '".$searchCriteria['status']."' ";
+		}
+		
+		// Set Group by
+		$groupField = "";
+		if(isset($searchCriteria['groupField']) && $searchCriteria['groupField'] != "")
+		{
+			$groupField = " GROUP BY ".$searchCriteria['groupField']." ";
+		}
+		
+		// Set Order Field
+		$orderField = " im.prod_id";
+		$orderDir = " ASC";
+		if(isset($searchCriteria['orderField']) && $searchCriteria['orderField'] != "")
+		{
+			$orderField = $searchCriteria['orderField'];
+		}
+		
+		// Set Group by
+		$groupField = "";
+		if(isset($searchCriteria['groupField']) && $searchCriteria['groupField'] != "")
+		{
+			$groupField = " GROUP BY ".$searchCriteria['groupField']." ";
+		}
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderDir']) && $searchCriteria['orderDir'] != "")
+		{
+			$orderDir = $searchCriteria['orderDir'];
+		}
+		
+		$sqlQuery = "SELECT ".$selectField." FROM inventory_master AS im
+					".$whereClaue." ".$groupField." ORDER BY ".$orderField." ".$orderDir."";
+		
+		//echo $sqlQuery; exit;
+		$result     = $this->db->query($sqlQuery);
+		$rsData     = $result->result_array();
+		return $rsData;	
+	}
 }
