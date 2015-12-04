a67665ec6264f208ce130b6b3ae6df6d03fab9be
Nikunj Patel <nikunjpatel190@gmail.com>
2015-12-04 18:33:15 +0530

    change

---
+++ application/frontend/controllers/inventory.php
@@ -0,0 +1,38 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class inventory extends CI_Controller {
+ 
+	function __construct()  
+	{
+		parent::__construct();
+		$this->load->model("orderModel",'',true);
+		$this->load->model("inventoryModel",'',true);
+		$this->load->model("processModel",'',true);
+		$this->load->model("productModel",'',true);
+	}
+	
+	public function index()
+	{
+	}
+
+	public function getInventoryDetail()
+	{
+		// Get inventory details
+		$searchCriteria = array();
+		$searchCriteria['selectField'] = "status,SUM(im.prod_qty) AS qty ";
+		$searchCriteria['prod_id'] = $this->Page->getRequest("prod_id");
+		$searchCriteria['groupField'] = "im.status";
+		$this->inventoryModel->searchCriteria = $searchCriteria;
+		$resultArr = $this->inventoryModel->getInventoryDetail();
+		
+		$inventoryDetailArr = array();
+		foreach($resultArr AS $row)
+		{
+			$inventoryDetailArr[$row['status']] = $row['qty'];
+		}
+		echo json_encode($inventoryDetailArr);
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
