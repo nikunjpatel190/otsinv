308c0dfe79b5c741b38b65d3f89ad1a650846891
Nikunj Patel <nikunjpatel190@gmail.com>
2015-12-10 18:01:44 +0530

    nikunj

--- application/frontend/views/product/assignProcessForm.php
+++ application/frontend/views/product/assignProcessForm.php
@@ -6,47 +6,92 @@
 	?>
 </div>
 
-<div class="row-fluid" id="printFrmDiv">
-     <div class="span12">
-     		<select class="required span2" name="slt_proc_typ" id="slt_proc_typ" >
-				<?php echo $this->Page->generateComboByTable("combo_master","combo_key","combo_value","","where combo_case='PROCESS_TYPE' order by seq",$rsEdit->ps_type,"Select Process Type"); ?>
-            </select>
-        	<table width="100%" cellpadding="5" cellspacing="5" border="0" class="table table-striped table-bordered table-hover dataTable">
-            <thead>
-                <tr class="hdr">
-                    <th class="span1 center">+/-</th>
-                    <th>User</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-				foreach($ProductsArr AS $row)
-				{
-				?>
-                	<tr id="row_<?php echo $row['prod_id']; ?>">
-                    	<td class="plus span1 center" id="<?php echo $row['prod_id']; ?>">+</td>
-                        <td>
-                        	<?php echo $row['prod_name']; ?>
-                        </td>
-                    </tr>
-                <?php
-				}
-				?>
-            </tbody>
-        </table>
+<div class="row-fluid">
+    <div class="span12">
+		<div class="control-group">
+			<div class="controls" id="chk_prod_typ">
+				<label>
+					<input type="radio" name="form-field-radio" checked="checked" value="">
+					<span class="lbl"> All</span>
+				</label>
+
+				<label>
+					<input type="radio" name="form-field-radio" value="product">
+					<span class="lbl"> Product</span>
+				</label>
+
+				<label>
+					<input type="radio" name="form-field-radio" value="component">
+					<span class="lbl"> Component</span>
+				</label>
+			</div>
+		</div>
+		<div class="clearfix"></div>
+		<div class="prodList">
+			<table width="100%" cellpadding="5" cellspacing="5" border="0" class="table table-striped table-bordered table-hover dataTable">
+				<thead>
+					<tr class="hdr">
+						<th class="span1 center">+/-</th>
+						<th>Product/Component Name</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					foreach($ProductsArr AS $row)
+					{
+					?>
+						<tr id="row_<?php echo $row['prod_id']; ?>">
+							<td class="plus span1 center" id="<?php echo $row['prod_id']; ?>">+</td>
+							<td>
+								<?php echo $row['prod_name']; ?>
+							</td>
+						</tr>
+					<?php
+					}
+					?>
+				</tbody>
+			</table>
+		</div>
     </div>
 </div>
-
-<!-- START GRID -->
-<div class="resp">
-</div>
-<!-- END GRID -->
 
 <?php include(APPPATH.'views/bottom.php'); ?>
 
 <script type="text/javascript">
 	$(document).ready(function(){
-		$(".plus").click(function(){
+		$(document).on("click","#chk_prod_typ input[type='radio']",function(){
+			var param = {};
+			param['prod_type'] = $(this).val();
+			param['res_type'] = "json";
+			var name = "";
+			if(param['prod_type']== 'product'){
+				name = "Product Name";	
+			}
+			else if(param['prod_type']=='component'){
+				name = "Component Name";
+			}
+			else{
+				name = "Product/Component Name";
+			}
+
+			$.ajax({
+					type:"POST",
+					url:"index.php?c=product&m=getProductDetail",
+					data:param,
+					dataType:"json",
+					beforeSend:function(){
+					},
+					success:function(res){
+						var html = '<table width="100%" cellpadding="5" cellspacing="5" border="0" class="table table-striped table-bordered table-hover dataTable"><thead><tr class="hdr"><th class="span1 center">+/-</th><th>'+name+'</th></tr></thead><tbody>';
+						$.each(res, function(k,v){
+							html += '<tr id="row_'+v.prod_id+'"><td class="plus span1 center" id="'+v.prod_id+'">+</td><td>'+v.prod_name+'</td></tr>';
+						});
+						html += '</tbody></table>';
+						$(".prodList").html(html);
+					}
+				});
+		});
+		$(document).on("click",".plus",function(){
 			var prod_id = this.id;
 			$(".resp").remove();
 			if($.trim($(this).html()) == '+')
