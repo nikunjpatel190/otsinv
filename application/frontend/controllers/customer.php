ce5332de5d1e2437603f5def64afde83e4a25c32
Nikunj Bambhroliya <nikunjpatel190@gmail.com>
2016-01-04 18:23:48 +0530

    change

---
+++ application/frontend/controllers/customer.php
@@ -0,0 +1,76 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class customer extends CI_Controller {
+ 
+    
+	function __construct()  
+	{
+		parent::__construct();
+		$this->load->model("customerModel",'',true);
+	}
+
+	// save Customer record
+	public function saveCustomer()
+	{	
+		// Check customer
+		$searchCriteria = array(); 
+		$searchCriteria["selectField"] = "cust_id,CONCAT(cust_first_name,' ',cust_last_name) AS name";
+		$searchCriteria["mobile"] = $_REQUEST['txt_cust_mobile'];
+		$this->customerModel->searchCriteria=$searchCriteria;
+		$rsCustomer = $this->customerModel->getCustomerDetails();
+		if(count($rsCustomer) > 0)
+		{
+			$option = '<option selected="selected" value='.$rsCustomer[0]['cust_id'].'>'.$rsCustomer[0]['name'].'</option>';
+			echo $option .= "|exist";
+		}
+		else
+		{
+			$arrData = array();
+			$arrData['cust_first_name'] = $_REQUEST['txt_cust_first_name'];
+			$arrData['cust_last_name'] = $_REQUEST['txt_cust_last_name'];
+			$arrData['cust_email'] = $_REQUEST['txt_cust_email'];
+			$arrData['cust_phone'] = $_REQUEST['txt_cust_phone'];
+			$arrData['cust_mobile'] = $_REQUEST['txt_cust_mobile'];
+			$arrData['cust_website'] = $_REQUEST['txt_cust_website'];
+			$arrData['shipping_street'] = $_REQUEST['txt_shipping_street'];
+			$arrData['shipping_city'] = $_REQUEST['txt_shipping_city'];
+			$arrData['shipping_state'] = $_REQUEST['txt_shipping_state'];
+			$arrData['shipping_zipcode'] = $_REQUEST['txt_cust_first_name'];
+			$arrData['shipping_country'] = $_REQUEST['txt_shipping_country'];
+
+			if(isset($_REQUEST['chk-copy-address']) && $_REQUEST['chk-copy-address'] == "on")
+			{
+				$arrData['billing_street'] = $_REQUEST['txt_shipping_street'];
+				$arrData['billing_city'] = $_REQUEST['txt_shipping_city'];
+				$arrData['billing_state'] = $_REQUEST['txt_shipping_state'];
+				$arrData['billing_zipcode'] = $_REQUEST['txt_shipping_zipcode'];
+				$arrData['billing_country'] = $_REQUEST['txt_shipping_country'];
+			}
+			else
+			{
+				$arrData['billing_street'] = $_REQUEST['txt_billing_street'];
+				$arrData['billing_city'] = $_REQUEST['txt_billing_city'];
+				$arrData['billing_state'] = $_REQUEST['txt_billing_state'];
+				$arrData['billing_zipcode'] = $_REQUEST['txt_billing_zipcode'];
+				$arrData['billing_country'] = $_REQUEST['txt_billing_country'];
+			}
+			$arrData['STATUS'] = 'ACTIVE';
+			$arrData['insertby'] =	$this->Page->getSession("intUserId");
+			$arrData['insertdate'] = date('Y-m-d H:i:s');
+			$this->customerModel->tbl = "customer_master";
+			$res=$this->customerModel->insert($arrData);
+			if($res > 0)
+			{
+				echo $res."|success";
+			}
+			else
+			{
+				echo "0|error";
+			}
+		}
+		exit;
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/frontend/models/customerModel.php
@@ -0,0 +1,88 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class customerModel extends Data {
+
+	public $searchCriteria;
+	function __construct() 
+	{
+        parent::__construct();
+        $this->tbl = 'customer_master';
+    }
+
+	function getCustomerDetails()
+	{
+		$searchCriteria = array();
+		$searchCriteria = $this->searchCriteria;
+		
+		$selectField = "*";
+		if(isset($searchCriteria['selectField']) && $searchCriteria['selectField'] != "")
+		{
+			$selectField = 	$searchCriteria['selectField'];
+		}
+		
+		$whereClaue = "WHERE 1=1 ";
+		
+		// By customer id
+		if(isset($searchCriteria['cusomerId']) && $searchCriteria['cusomerId'] != "")
+		{
+			$whereClaue .= 	" AND cust_id=".$searchCriteria['userType']." ";
+		}
+		
+		// By customer name
+		if(isset($searchCriteria['name']) && $searchCriteria['name'] != "")
+		{
+			$whereClaue .= 	" AND CONCAT(cust_first_name,' ',cust_last_name) like '%".$searchCriteria['name']."%' ";
+		}
+		
+		// By customer email
+		if(isset($searchCriteria['email']) && $searchCriteria['email'] != "")
+		{
+			$whereClaue .= 	" AND cust_email='".$searchCriteria['email']."' ";
+		}
+
+		// By mobile number
+		if(isset($searchCriteria['mobile']) && $searchCriteria['mobile'] != "")
+		{
+			$whereClaue .= 	" AND cust_mobile='".$searchCriteria['mobile']."' ";
+		}
+		
+		// By Status
+		if(isset($searchCriteria['status']) && $searchCriteria['status'] != "")
+		{
+			$whereClaue .= 	" AND status='".$searchCriteria['status']."' ";
+		}
+		
+		// Not In
+		if(isset($searchCriteria['not_id']) && $searchCriteria['not_id'] != "")
+		{
+			$whereClaue .= 	" AND cust_id !=".$searchCriteria['not_id']." ";
+		}
+		
+		$orderField = " cust_first_name";
+		$orderDir = " ASC";
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderField']) && $searchCriteria['orderField'] != "")
+		{
+			$orderField = $searchCriteria['orderField'];
+		}
+		
+		// Set Order Field
+		if(isset($searchCriteria['orderDir']) && $searchCriteria['orderDir'] != "")
+		{
+			$orderDir = $searchCriteria['orderDir'];
+		}
+		
+		
+		$sqlQuery = "SELECT 
+						".$selectField."
+					FROM 
+						customer_master ".$whereClaue." ORDER BY ".$orderField." ".$orderDir."";
+		//echo $sqlQuery; exit;
+		$result     = $this->db->query($sqlQuery);
+		$rsData     = $result->result_array();
+		return $rsData;
+	}
+}
