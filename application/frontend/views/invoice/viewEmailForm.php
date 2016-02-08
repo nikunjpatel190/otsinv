73a69ea87729fe7082f5537567ec6fae4e761583
Nikunj Bambhroliya <nikunjpatel190@gmail.com>
2016-02-08 19:47:45 +0530

    change

--- application/frontend/views/invoice/viewEmailForm.php
+++ application/frontend/views/invoice/viewEmailForm.php
@@ -1,69 +1,64 @@
 <?php include(APPPATH.'views/top.php');
 $this->load->helper('form');
-$attributes = array('class' => 'frm_add_record form-horizontal', 'id' => 'frmMailInvoice', 'name' => 'frmMailInvoice');
+$attributes = array('class' => 'form-horizontal', 'id' => 'frmMailInvoice', 'name' => 'frmMailInvoice');
 echo form_open('c=invoice&m=send_mail', $attributes);
- ?>
-
+?>
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300" />
-<input type="hidden" name="orderId" id="orderId" value="<?php echo $orderDetailArr["order_id"]; ?>">
-
 <div class="main-container container-fluid">
 <div class="page-header position-relative">
     <h1>Send Invoice</h1>
 </div>
-    <div class="row-fluid">
-        	<div class="span12">
-                    <div class="control-group">
-                        <label class="control-label" for="form-field-1">To :</label>
+<div class="row-fluid">
+	<div class="span12">
+		<div class="control-group">
+			<label class="control-label" for="form-field-1">To :</label>
 
-                        <div class="controls">
-                            <input type="text" id="form-field-1" name="to" placeholder="To" class="span10"/>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="form-field-1">From :</label>
+			<div class="controls">
+				<input type="text" id="to" name="to" placeholder="To" class="span10 required isemail"/>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="form-field-1">From :</label>
 
-                        <div class="controls">
-                            <input type="text" id="form-field-1" name="from" placeholder="From" class="span10"/>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="form-field-1">Cc :</label>
+			<div class="controls">
+				<input type="text" id="from" name="from" placeholder="From" class="span10 required isemail"/>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="form-field-1">Cc :</label>
 
-                        <div class="controls">
-                            <input type="text" id="form-field-1" name="cc" placeholder="Cc" class="span10"/>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="form-field-1">Message :</label>
+			<div class="controls">
+				<input type="text" id="form-field-1" name="cc" placeholder="Cc" class="span10"/>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="form-field-1">Message :</label>
 
-                        <div class="controls">
-                           <div class="span10">
-                           		 <div class="wysiwyg-editor" id="editor1"></div>
-                                 <input type="hidden" name="hideditor"  id="hideditor"/>
-                           </div>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="form-field-1">Select File :</label>
+			<div class="controls">
+			   <div class="span10">
+					 <div class="wysiwyg-editor" id="editor1"></div>
+					 <input type="hidden" name="hideditor"  id="hideditor"/>
+			   </div>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="form-field-1">Select File :</label>
 
-                        <div class="controls">
-                            <input type="file" id="form-field-1" placeholder="Cc" class="span10"/>
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="form-field-1">&nbsp;</label>
+			<div class="controls">
+				<input type="file" id="form-field-1" placeholder="Cc" class="span10"/>
+			</div>
+		</div>
+		<div class="control-group">
+			<label class="control-label" for="form-field-1">&nbsp;</label>
 
-                        <div class="controls">
-                            <button type="submit" class="btn btn-small btn-primary" id="sendMail">
-                                <i class="icon-ok"></i>
-                                Send Mail
-                            </button>
-                        </div>
-                    </div>
-            	
-               
-        </div>
+			<div class="controls">
+				<button type="submit" class="btn btn-small btn-primary" id="sendMail" name="sendMail">
+					<i class="icon-ok"></i>
+					Send Mail
+				</button>
+			</div>
+		</div>   
+	</div>
 </div>
 <?php echo form_close(); ?>
 <?php include(APPPATH.'views/bottom.php'); ?>
@@ -77,12 +72,24 @@ $(document).ready(function(){
 		var text = $('#editor1').text();
 		$('#hideditor').val(text);
 		$('#frmMailInvoice').submit();
-		
     });
 });
 
-	$(function(){
-	
+$(document).on('submit','#frmMailInvoice',function(e){
+	  //e.preventDefault();
+	  $(".errmsg").remove();
+	  if(!submit_form(this))
+	  {
+			return false;
+	  }
+
+	  if($('#hideditor').val() == "")
+	  {
+		  alert("Please enter message"); return false;
+	  }
+});
+
+$(function(){
 	function showErrorAlert (reason, detail) {
 		var msg='';
 		if (reason==='unsupported-file-type') { msg = "Unsupported format " +detail; }
@@ -132,45 +139,6 @@ $(document).ready(function(){
 			fileUploadError: showErrorAlert
 		}
 	}).prev().addClass('wysiwyg-style2');
-
-	
-
-	$('#editor2').css({'height':'200px'}).ace_wysiwyg({
-		toolbar_place: function(toolbar) {
-			return $(this).closest('.widget-box').find('.widget-header').prepend(toolbar);
-		},
-		toolbar:
-		[
-			'bold',
-			{name:'italic' , title:'Change Title!', icon: 'icon-leaf'},
-			'strikethrough',
-			'underline',
-			null,
-			'insertunorderedlist',
-			'insertorderedlist',
-			null,
-			'justifyleft',
-			'justifycenter',
-			'justifyright'
-		],
-		speech_button:false
-	});
-
-
-	$('[data-toggle="buttons-radio"]').on('click', function(e){
-		var target = $(e.target);
-		var which = parseInt($.trim(target.text()));
-		var toolbar = $('#editor1').prev().get(0);
-		if(which == 1 || which == 2 || which == 3) {
-			toolbar.className = toolbar.className.replace(/wysiwyg\-style(1|2)/g , '');
-			if(which == 1) $(toolbar).addClass('wysiwyg-style1');
-			else if(which == 2) $(toolbar).addClass('wysiwyg-style2');
-		}
-	});
-
-
-
-
 
 	//Add Image Resize Functionality to Chrome and Safari
 	//webkit browsers don't have image resize functionality when content is editable
@@ -234,6 +202,6 @@ $(document).ready(function(){
 
 
 });
-		</script>
+</script>
         
   
