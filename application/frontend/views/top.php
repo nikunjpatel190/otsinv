604e58c629f012ba5915c4e374611dc01a3b8c24
snehal trapsiya <snehal.trapsiya@gmail.com>
2016-02-15 11:17:39 +0530

    change

--- application/frontend/views/top.php
+++ application/frontend/views/top.php
@@ -3,6 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   		<meta name="description" content="overview &amp; stats" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <title>Order Management System</title>
         <link href="./css/bootstrap.min.css" rel="stylesheet" />
         <link href="./css/bootstrap-responsive.min.css" rel="stylesheet" />
